8dc2385891aee91492d482c50a089f539c0d2689
Pablo Barbosa Silva <126425360+PabloPBS@users.noreply.github.com>
2023-05-13 20:49:18 -0300

    Conversor de Temperaturas

---
+++ Java POO/AplicacoesJava/ConversorTemperaturas/src/App.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Scanner scan = new Scanner(System.in);
+        double celsius;
+        double fahrenheit;
+        double kelvin;
+        String res;
+
+        do {
+            System.out.print("Digite a temperatua a ser convertida (em ºC): ");
+            celsius = scan.nextDouble();
+
+            System.out.println("\nTemperatura escolhida: " + celsius + "ºC");
+
+            fahrenheit = celsius * 1.8 + 3;
+            kelvin = celsius + 273.15;
+
+            System.out.printf("Temperatura em Fahrenheit: %.2fºF%n", kelvin);
+            System.out.printf("Temperatura em Kevlin: %.2fºK%n", fahrenheit);
+
+            System.out.print("Deseja converter outra temperatura? (S/N) ");
+            res = scan.next();
+            System.out.println("-=-=-");
+        } while (res.equals("S") || res.equals("s"));
+
+        System.out.println("Encerrando programa...");
+    }
+}
